999ba63ab22ba938e74e94e2232c9a59b380c6e4
Liam Marshall <liam@cpan.org>
2015-06-29 18:46:15 -0500

    Function for determining admin permissions

--- setup.py
+++ setup.py
@@ -4,3 +4,17 @@ import os
 import sys
 import platform
 import subprocess
+
+def has_admin():
+    if platform.system() == 'Windows':
+        try:
+            # only windows users with admin privileges can read the C:\windows\temp
+            temp = os.listdir(os.sep.join([os.environ.get('SystemRoot','C:\\windows'),'temp']))
+        except:
+            return False
+        else:
+            return True
+    else:
+    	if os.geteuid() == 0:
+    		return True
+
